c87dc143d4cc7df59a9136b69842260e8a2639cb
Nathan Zou <77636883+AgentSlimy@users.noreply.github.com>
2021-11-29 22:19:31 -0800

    Update Classes.cpp

--- Classes.cpp
+++ Classes.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
+
 using namespace std;
+
+int main() {
+	bool running = true;
+	char input[10];
+	char title[30];
+	int year;
+	cout << "Welcome to Classes." << endl;
+	while(running == true) {
+
+	}
+}
