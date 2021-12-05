6e15539b0548119573d54bb5951b225559580021
Nathan Zou <77636883+AgentSlimy@users.noreply.github.com>
2021-12-05 13:43:19 -0800

    Update music.h

--- music.h
+++ music.h
@@ -8,15 +8,15 @@
 using namespace std;
 
 class Music : public Media {
- public:
-  music();
-  virtual int getType();
-  char* getArtist();
-  char* getPublisher();
-  int* getDuration();
- private:
-  char artistMusic[30];
-  char publisherMusic[30];
-  int durationMusic;
+public:
+	Music();
+	virtual int getType();
+	char* getArtist();
+	char* getPublisher();
+	int* getDuration();
+private:
+	char artistMusic[30];
+	char publisherMusic[30];
+	int durationMusic;
 };
 #endif
