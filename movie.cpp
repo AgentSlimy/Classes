8bab243a33d1d980b062f9113c697706eb6ad51d
Nathan Zou <77636883+AgentSlimy@users.noreply.github.com>
2021-12-08 11:36:01 -0800

    Added comments for child classes

--- movie.cpp
+++ movie.cpp
@@ -10,15 +10,15 @@ Movie::Movie() {
 }
 
 char* Movie::getDirector() {
-	return directorMovie;
+	return directorMovie; //Returns Director from "movie.h" class
 }
 
 int* Movie::getDuration() {
-	return &durationMovie;
+	return &durationMovie; //Returns Duration from "movie.h" class
 }
 
 float* Movie::getRating() {
-	return &ratingMovie;
+	return &ratingMovie; //Returns Rating from "movie.h" class
 }
 
 int Movie::getType() {
