a2390d936d4b625dc4f47f1c005130a5d14a28e6
Nathan Zou <77636883+AgentSlimy@users.noreply.github.com>
2021-12-08 11:33:20 -0800

    Added comments to child classes

--- music.h
+++ music.h
@@ -11,12 +11,12 @@ class Music : public Media {
 public:
 	Music();
 	virtual int getType();
-	char* getArtist();
-	char* getPublisher();
-	int* getDuration();
+	char* getArtist(); //Get song artist char pointer
+	char* getPublisher(); //Get song publisher char pointer
+	int* getDuration(); //Get song duration int pointer
 private:
-	char artistMusic[30];
-	char publisherMusic[30];
-	int durationMusic;
+	char artistMusic[30]; //Artist input array
+	char publisherMusic[30]; //Publisher input array
+	int durationMusic; //Int for duration of music in seconds
 };
 #endif
